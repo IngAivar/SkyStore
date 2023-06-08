acd9f3bd874ff89ee1e71160edaf57df21f21ab9
MiniGan067 <MiniGan067@yandex.ru>
2023-06-08 15:46:55 +0300

    Bunch of work!!

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app_newsletter.urls', namespace='app_newsletter'))
 ]
 
 if settings.DEBUG:
